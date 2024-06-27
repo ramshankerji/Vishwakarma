a60cd7295f67063d4549cb255afa60617924316a
Ram Shanker <ram@ramshanker.in>
2024-06-27 22:41:04 +0530

    zlib and pnglib submodule added. Yet to configure properly.

--- api-direct3d12/Main.cpp
+++ api-direct3d12/Main.cpp
@@ -10,6 +10,84 @@
 #include <iostream>
 #include <vishwakarma-2D.h>
 #include <random>
+#include <png.h>
+
+#pragma comment(lib, "libpng16.lib")
+#pragma comment(lib, "zlib.lib")
+
+
+void LoadPngImage(const char* filename, unsigned char** image_data, int* width, int* height)
+{
+    /*
+    FILE* fp = fopen(filename, "rb");
+    if (!fp) abort();
+
+    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png) abort();
+
+    png_infop info = png_create_info_struct(png);
+    if (!info) abort();
+
+    if (setjmp(png_jmpbuf(png))) abort();
+
+    png_init_io(png, fp);
+
+    png_read_info(png, info);
+
+    *width = png_get_image_width(png, info);
+    *height = png_get_image_height(png, info);
+    png_byte color_type = png_get_color_type(png, info);
+    png_byte bit_depth = png_get_bit_depth(png, info);
+
+    if (bit_depth == 16)
+        png_set_strip_16(png);
+
+    if (color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(png);
+
+    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png);
+
+    if (png_get_valid(png, info, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png);
+
+    if (color_type == PNG_COLOR_TYPE_RGB ||
+        color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+    if (color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(png);
+
+    png_read_update_info(png, info);
+
+    *image_data = (unsigned char*)malloc(png_get_rowbytes(png, info) * (*height));
+    png_bytep* row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * (*height));
+    for (int y = 0; y < *height; y++) {
+        row_pointers[y] = (*image_data) + y * png_get_rowbytes(png, info);
+    }
+
+    png_read_image(png, row_pointers);
+
+    fclose(fp);
+    free(row_pointers);
+    png_destroy_read_struct(&png, &info, NULL);
+    */
+}
+
+void DisplayImage(HDC hdc, const unsigned char* image_data, int width, int height)
+{
+    BITMAPINFO bmi = { 0 };
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = width;
+    bmi.bmiHeader.biHeight = -height; // top-down DIB
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    StretchDIBits(hdc, 0, 0, width, height, 0, 0, width, height, image_data, &bmi, DIB_RGB_COLORS, SRCCOPY);
+}
 
 // Global variables
 
--- code-external/README.md
+++ code-external/README.md
@@ -6,5 +6,23 @@ To add a new submodule, go to root of Vishwakarma and give following commands.
 Ex: For Freetype Library with Release Tag No. VER-2-3-2
 
 git submodule add https://gitlab.freedesktop.org/freetype/freetype.git code-external/freetype
+
 cd code-external/freetype
+
 git checkout tags/VER-2-13-2
+
+
+Most of these libraries expect to be built on the local machine after download.
+Usually this is done by following commands:
+
+cd libraryname
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
+
+
+libpng build Instructions:
+cmake -D ZLIB_INCLUDE_DIR=../../zlib -D ZLIB_LIBRARY=../../zlib/build/Debug ..
+
+cmake --build . --config Release
